4e2c465b7326318648bc3865994ea0ca7aa88c71
Andy Mitchell <me@themitchell.co.uk>
2017-05-04 13:24:04 +0100

    Docker NUKE 💥

--- .bash_partials/docker.sh
+++ .bash_partials/docker.sh
@@ -1,1 +1,3 @@
 if which docker-machine > /dev/null; then eval $(docker-machine env default); fi
+
+alias docker-nuke="docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q) && docker rmi $(docker images -aq)"
