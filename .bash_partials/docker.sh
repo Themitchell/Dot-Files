01358f42e65079b1b23ece06289f3c9f72c8df1b
Andy Mitchell <me@themitchell.co.uk>
2017-05-10 15:38:07 +0100

    Docker nuke should be a function

--- .bash_partials/docker.sh
+++ .bash_partials/docker.sh
@@ -1,3 +1,5 @@
 if which docker-machine > /dev/null; then eval $(docker-machine env default); fi
 
-alias docker-nuke="docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q) && docker rmi $(docker images -aq)"
+docker_nuke() {
+  docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q) && docker rmi $(docker images -aq)
+}
