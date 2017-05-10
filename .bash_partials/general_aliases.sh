3e63123e207f8169e254754a808a1b158dda7c6c
Andy Mitchell <me@themitchell.co.uk>
2017-05-10 15:38:18 +0100

    Improved aliases

--- .bash_partials/general_aliases.sh
+++ .bash_partials/general_aliases.sh
@@ -1,4 +1,14 @@
 # General Aliases
 
 alias ls='ls -F'
-alias ll='ls -lah'
+alias ll='ls -lAh'
+
+alias psg="ps aux | grep -v grep | grep -i -e VSZ -e"
+
+alias mkdir="mkdir -pv"
+
+alias wget="wget -c"
+
+alias ip="curl http://ipecho.net/plain; echo"
+
+
