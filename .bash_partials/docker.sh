f5b247b9fa52812121f52b807bc5f137a931fc99
Andy Mitchell <me@themitchell.co.uk>
2018-03-21 19:28:59 +0000

    Improve docker file

--- .bash_partials/docker.sh
+++ .bash_partials/docker.sh
@@ -1,5 +1,7 @@
-if which docker-machine > /dev/null; then eval $(docker-machine env default); fi
+# if which docker-machine > /dev/null; then eval $(docker-machine env default); fi
 
 docker_nuke() {
-  docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q) && docker rmi $(docker images -aq)
+  docker stop $(docker ps -a -q) && \
+  docker rm $(docker ps -a -q) && \
+  docker rmi $(docker images -aq)
 }
