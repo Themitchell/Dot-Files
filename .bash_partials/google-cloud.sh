446c2305480c773d10e54247333a79edddc9b117
Andy Mitchell <me@themitchell.co.uk>
2019-08-01 09:03:45 +0100

    update google cloud for use with homebrew

--- .bash_partials/google-cloud.sh
+++ .bash_partials/google-cloud.sh
@@ -1,7 +1,7 @@
 # The next line updates PATH for the Google Cloud SDK.
-if [ -f '/usr/local/bin/google-cloud-sdk/path.bash.inc' ]; then source '/usr/local/bin/google-cloud-sdk/path.bash.inc'; fi
+if [ -f '/usr/local/bin/google-cloud-sdk/path.bash.inc' ]; then source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.inc'; fi
 
 # The next line enables shell command completion for gcloud.
-if [ -f '/usr/local/bin/google-cloud-sdk/completion.bash.inc' ]; then source '/usr/local/bin/google-cloud-sdk/completion.bash.inc'; fi
+if [ -f '/usr/local/bin/google-cloud-sdk/completion.bash.inc' ]; then source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc'; fi
 
-export APPENGINE_PATH="/usr/local/bin/google-cloud-sdk/platform/google_appengine"
+export APPENGINE_PATH='/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/platform/google_appengine'
